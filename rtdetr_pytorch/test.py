874def81867f4a92ca74bbe858a497444a96716f
Amin <karimimonsefi.1@osu.edu>
2024-05-30 09:15:42 -0400

    add test file

--- rtdetr_pytorch/test.py
+++ rtdetr_pytorch/test.py
@@ -1,3 +1,6 @@
 from src.core.yaml_config import YAMLConfig
+from src.solver.det_solver import DetSolver
 
 test_c = YAMLConfig("/Users/amin/Desktop/higharc/RT-DETR-plan/rtdetr_pytorch/configs/rtdetr/rtdetr_r50vd_m_6x_coco_segmentation_higharc.yml")
+
+sol = DetSolver(test_c)
